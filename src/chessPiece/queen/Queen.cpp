46d9e3ee6f557d00e60617aaa661f19d2161e7a8
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2024-04-13 17:07:17 +0200

    Queen pseudo-valid moves

--- src/chessPiece/queen/Queen.cpp
+++ src/chessPiece/queen/Queen.cpp
@@ -11,5 +11,39 @@
 Queen::Queen(ChessPieceType type, ChessPieceColor color, Game* instance, int x, int y) : ChessPiece(type, color, instance, x, y) {}
 
 std::vector<Coords> Queen::getValidMoves() {
-  return {};
+  std::vector<Coords> moves;
+
+  int _x = this->getX();
+  int _y = this->getY();
+
+  // Pseudo-valid
+  for (int i = 1; i < 8; ++i) {
+    // Right & up (x+, y+)
+    if (_x + i < 8 && _y + i < 8)
+      moves.emplace_back(_x + i, _y + i);
+
+    // Right & down (x+, y-)
+    if (_x + i < 8 && _y - i >= 0)
+      moves.emplace_back(_x + i, _y - i);
+
+    // Left & up (x-, y+)
+    if (_x - i >= 0 && _y + i < 8)
+      moves.emplace_back(_x - i, _y + i);
+
+    // Left & down (x-, y-)
+    if (_x - i >= 0 && _y - i >= 0)
+      moves.emplace_back(_x - i, _y - i);
+
+    // Horizontal
+    if (i != this->getX()) {
+      moves.emplace_back(i, this->getY());
+    }
+
+    // Vertical
+    if (i != this->getY()) {
+      moves.emplace_back(this->getX(), i);
+    }
+  }
+
+  return moves;
 }
