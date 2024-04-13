3b8057a63947376ac02d8f594751807669cc6cff
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2024-04-13 17:12:35 +0200

    Knight pseudo-valid moves

--- src/chessPiece/knight/Knight.cpp
+++ src/chessPiece/knight/Knight.cpp
@@ -11,5 +11,22 @@
 Knight::Knight(ChessPieceType type, ChessPieceColor color, Game* instance, int x, int y) : ChessPiece(type, color, instance, x, y) {}
 
 std::vector<Coords> Knight::getValidMoves() {
-  return std::vector<Coords>();
+  std::vector<Coords> moves;
+  // Pseudo-valid
+
+  int _x = this->getX();
+  int _y = this->getY();
+
+  int deltaX[] = {1, 1, 2, 2, -1, -1, -2, -2};
+  int deltaY[] = {2, -2, 1, -1, 2, -2, 1, -1};
+
+  for (int i = 0; i < 8; ++i) {
+    int newX = _x + deltaX[i];
+    int newY = _y + deltaY[i];
+    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+      moves.emplace_back(newX, newY);
+    }
+  }
+
+  return moves;
 }
