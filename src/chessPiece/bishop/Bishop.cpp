3426fd6fda5d6ca18a699c43f067945ca0e3d5d8
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2024-04-14 13:25:21 +0200

    Bishop actual-valid moves

--- src/chessPiece/bishop/Bishop.cpp
+++ src/chessPiece/bishop/Bishop.cpp
@@ -13,27 +13,40 @@ ChessPiece(type, color, instance, x, y) {}
 
 std::vector<Coords> Bishop::getValidMoves(ChessPiece* board[8][8]) {
   std::vector<Coords> moves;
-
   int _x = this->getX();
   int _y = this->getY();
+  ChessPieceColor color = this->getColor();
 
-  // Pseudo-valid
+  // Define helper function to check if a square is valid to move to
+  auto isValidMove = [&](int x, int y) {
+    return x >= 0 && x < 8 && y >= 0 && y < 8 &&
+        (board[x][y] == nullptr || board[x][y]->getColor() != color);
+  };
+
+  // Check diagonal moves (top-right, top-left, bottom-right, bottom-left)
   for (int i = 1; i < 8; ++i) {
-    // Right & up (x+, y+)
-    if (_x + i < 8 && _y + i < 8)
-      moves.emplace_back(_x + i, _y + i);
-
-    // Right & down (x+, y-)
-    if (_x + i < 8 && _y - i >= 0)
-      moves.emplace_back(_x + i, _y - i);
-
-    // Left & up (x-, y+)
-    if (_x - i >= 0 && _y + i < 8)
-      moves.emplace_back(_x - i, _y + i);
-
-    // Left & down (x-, y-)
-    if (_x - i >= 0 && _y - i >= 0)
-      moves.emplace_back(_x - i, _y - i);
+    // Top-right
+    if (!isValidMove(_x + i, _y + i)) break;
+    moves.emplace_back(_x + i, _y + i);
+    if (board[_x + i][_y + i] != nullptr) break;
+  }
+  for (int i = 1; i < 8; ++i) {
+    // Top-left
+    if (!isValidMove(_x + i, _y - i)) break;
+    moves.emplace_back(_x + i, _y - i);
+    if (board[_x + i][_y - i] != nullptr) break;
+  }
+  for (int i = 1; i < 8; ++i) {
+    // Bottom-right
+    if (!isValidMove(_x - i, _y + i)) break;
+    moves.emplace_back(_x - i, _y + i);
+    if (board[_x - i][_y + i] != nullptr) break;
+  }
+  for (int i = 1; i < 8; ++i) {
+    // Bottom-left
+    if (!isValidMove(_x - i, _y - i)) break;
+    moves.emplace_back(_x - i, _y - i);
+    if (board[_x - i][_y - i] != nullptr) break;
   }
 
   return moves;
