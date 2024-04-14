4a75b9fd4f5bf737b4f6dbd732260e01679e225a
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2024-04-14 13:37:24 +0200

    Knight actual-valid moves

--- src/chessPiece/knight/Knight.cpp
+++ src/chessPiece/knight/Knight.cpp
@@ -15,15 +15,24 @@ std::vector<Coords> Knight::getValidMoves(ChessPiece* board[8][8]) {
 
   int _x = this->getX();
   int _y = this->getY();
+  ChessPieceColor color = this->getColor();
 
-  // Pseudo-valid
+  // Define helper function to check if a square is valid to move to
+  auto isValidMove = [&](int x, int y) {
+    // If the move is within bounds and
+    // (the cell in question is empty or
+    // the color of the not empty cell is not the same as the color of the current piece)
+    return x >= 0 && x < 8 && y >= 0 && y < 8 &&
+        (board[x][y] == nullptr || board[x][y]->getColor() != color);
+  };
+
   int deltaX[] = {1, 1, 2, 2, -1, -1, -2, -2};
   int deltaY[] = {2, -2, 1, -1, 2, -2, 1, -1};
 
   for (int i = 0; i < 8; ++i) {
     int newX = _x + deltaX[i];
     int newY = _y + deltaY[i];
-    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+    if (isValidMove(newX, newY)) {
       moves.emplace_back(newX, newY);
     }
   }
