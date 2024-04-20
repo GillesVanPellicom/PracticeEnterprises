7113e19fd72c107a7303e6eba466c88ed2f01496
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2024-04-20 21:39:51 +0200

    Updated check

--- src/Game.h
+++ src/Game.h
@@ -292,8 +292,7 @@ class Game final : public ChessWindow {
      * @param piece Piece to be examined
      * @return true if check, false if not
      */
-    bool isCheck(ChessPiece* piece);
-
+    bool isCheck(int x, int y, ChessPieceColor attackerColor);
     /**
      * Examines the entire board and both sides for checks.
      * Will modify global variables blackInCheck and whiteInCheck after call.
@@ -320,7 +319,8 @@ class Game final : public ChessWindow {
     // ChessPiece* board[8][8]{};
     std::array<std::array<ChessPiece*, 8>, 8> board;
 
-  private:
+  private
+  :
     // Current selected cell variables
     bool isSelected;
     Coords selected = {-1, -1};
